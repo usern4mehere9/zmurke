f9ba203133c5617f541d6375061c0576e133a5ab
usern4mehere9 <91462628+usern4mehere9@users.noreply.github.com>
2021-10-02 21:42:29 +0200

    Add files via upload

---
+++ zmurke/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace zmurke
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, nt;
+
+            n = int.Parse(Console.ReadLine());
+
+            nt = (n * 5) % 100;
+
+            Console.WriteLine(nt);
+        }
+    }
+}
